02a634aac6fa67d35dd1ee31e1453b47e8469576
Taylor Eisman <TaylorEisman@fullsail.edu>
2012-08-28 19:44:31 -0500

    Adding Recipes into JSON data

--- recipeStalker/javascript/json.js
+++ recipeStalker/javascript/json.js
@@ -4,9 +4,49 @@
 
 var json = {
 	"recipe1": {
-		"category": ["Category:", "Lunch"],
-		"ingredient1": [" :", ""],
-		"": [" :", ""],
+        "recipeName": ["Recipe Name:", "Bacon Wraps"],
+		"category": ["Category:", "Appetizers"],
+		"ingredient1": ["Ingrediant:", "Cocktail sausages"],
+		"measurement1": ["1", "package"],
+        "ingredient2": ["Ingrediant", "Bacon"],
+        "measurement2": ["1", "pound"],
+        "ingrediant3": ["Ingrediant", "Brown sugar"],
+        "measurement3": ["1", "Cups"],
+        "addToFav": ["Favorite:", "No"],
+        "prepTime": ["15", "Minutes"],
+        "dateAdded": ["Date Added:", "08/28/2012"],
+        "instructions": ["Instructions:", "Cut slab of bacon into quarters. Wrap one piece of quartered bacon slice around each sausage weenie. Place on foil lined cookie sheet. Sprinkle brown sugar on top of each wrapped sausage. Bake at 400 degrees for 30-40 minutes"]
 	},
-
+    "recipe2": {
+        "recipeName": ["Recipe Name:", "Rock N Roll Crackers"],
+        "category": ["Category:", "Appetizers"],
+        "ingredient1": ["Ingrediant:", "Ranch salad dressing seasoning mix"],
+        "measurement1": ["1", "Package"],
+        "ingredient2": ["Ingrediant", "Crushed red pepper"],
+        "measurement2": ["1 1/2", "Tablespoon"],
+        "ingrediant3": ["Ingrediant", "Garlic powder"],
+        "measurement3": ["1", "Teaspoon"],
+        "ingrediant4": ["Ingrediant", "Saltine crackers"],
+        "measurement4": ["1", "Package"],
+        "ingrediant5": ["Ingrediant", "Canola oil"],
+        "measurement5": ["1 1/2", "Cups"],
+        "addToFav": ["Favorite:", "No"],
+        "prepTime": ["20", "Minutes"],
+        "dateAdded": ["Date Added:", "08/28/2012"],
+        "instructions": ["Instructions:", "In a small bowl mix together salad seasoning mix, red pepper flakes, and garlic powder. Set aside. Place crackers in a large gallon sized sealable container, drizzle with canola oil. Add dry ingredients and seal container. Gently roll container to coat crackers with seasonings. Gently roll crackers every 30 minutes for 4 hours for best results."]
+    },
+    "recipe3": {
+        "recipeName": ["Recipe Name:", "Sausage Cheese Balls"],
+        "category": ["Category:", "Appetizers"],
+        "ingredient1": ["Ingrediant:", "Pan sausage"],
+        "measurement1": ["1", "Pound"],
+        "ingredient2": ["Ingrediant", "Bisquick"],
+        "measurement2": ["2", "Cups"],
+        "ingrediant3": ["Ingrediant", "Grated cheddar cheese"],
+        "measurement3": ["8", "Ounces"],
+        "addToFav": ["Favorite:", "Yes"],
+        "prepTime": ["15", "Minutes"],
+        "dateAdded": ["Date Added:", "08/28/2012"],
+        "instructions": ["Instructions:", "Mix all ingredients well and form into walnut sized balls. Place on cookie sheet sprayed with Pam or non stick spray. Bake at 370 degrees for 15-20 minutes depending on the size of balls. Can be frozen and heated before serving."]
+    },
 }
