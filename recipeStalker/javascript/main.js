22c91a15928d5a4b9456c51496aab15faf04c70b
Taylor Eisman <TaylorEisman@fullsail.edu>
2012-08-30 02:11:11 -0500

    added autofill and getdata functions to main.js

--- recipeStalker/javascript/main.js
+++ recipeStalker/javascript/main.js
@@ -25,10 +25,45 @@ $('#addItem').on('pageinit', function(){
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
 var autofillData = function (){
-	 
+	 .each(var n in json){
+			var id = Math.floor(Math.random()*100000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
 };
 
 var getData = function(){
+	$("#addRecipeForm").empty();
+		toggleControls("on");
+		if(localStorage.length === 0){
+			alert("There is no Local Storage so default data was added.");
+			autoFillData();
+		}
+		var makeDiv = $('<div>');
+		makeDiv.attr("id", "items");
+		var makeList = $('<ul>');
+		makeDiv.append(makeList);
+		document.body.appendChild(makeDiv);
+		$('#recipes').append(makeDiv);
+		$('#items').show();
+		for(var i=0, len=localStorage.length; i<len;i++){
+			var makeli = $('<li>');
+			var linksLi = $('<li>');
+			makeList.append(makeli);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var obj = JSON.parse(value);
+			var makeSubList = $('<ul>');
+			makeli.append(makeSubList);
+			getImage(obj.category[1], makeSubList);
+			for(var n in obj){
+				var makeSubli = $('<li>');
+				makeSubList.append(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.text(optSubText);
+				makeSubList.append(linksLi);
+			}
+			makeItemLinks(localStorage.key(i), linksLi);
+		}
 
 };
 
