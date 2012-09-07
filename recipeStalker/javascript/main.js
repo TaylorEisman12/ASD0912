0ee6b463ae39be95d6f069c6d770827ef3c5d839
Taylor Eisman <TaylorEisman@fullsail.edu>
2012-09-06 21:57:52 -0500

    Ooops :( have not been commiting my main.js :(

--- recipeStalker/javascript/main.js
+++ recipeStalker/javascript/main.js
@@ -1,6 +1,6 @@
 // Taylor Eisman
 // ASD 09/12
-// Project 1
+// Project 2
 
 // comments function   add comments for recipes
 // groceryList function set up pop-up list with checkbox populated with ingrediants to add only needed ingrediants
@@ -8,7 +8,132 @@
 
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
-});	
+});
+    //<name>
+    //<category>
+    //<ingredient1>
+    //<measurement1>
+    //<ingredient2>
+    //<measurement2>
+    //<ingredient3>
+    //<measurement3>
+    //<favorite>
+    //<prep>
+    //<date>
+    //<instructions>
+$('#dataXML').on('pageinit', function(){
+    var xmlData = function () {
+        $.ajax({
+            type: "GET",
+            url: "xhr/data.xml",
+            dataType: "xml",
+            success: function (dataXML) {
+                $(dataXML).find('item').each(function() {
+                        var name = $(this).find('name').text();
+                        var category = $(this).find('category').text();
+                        var ingredient1 = $(this).find('ingredient1').text();
+                        var measurement1 = $(this).find('measurement1').text();
+                        var ingredient2 = $(this).find('ingredient2').text();
+                        var measurement2 = $(this).find('measurement2').text();
+                        var ingredient3 = $(this).find('ingredient3').text();
+                        var measurement3 = $(this).find('measurement3').text();
+                        var favorite = $(this).find('favorite').text();
+                        var prep = $(this).find('prep').text();
+                        var date = $(this).find('date').text();
+                        var instructions = $(this).find('instructions').text();
+                        $(' '+
+                            '<div class="contentXML">' +
+                                '<ul>' +
+                                    '<li> Recipe: ' + name + '</li>' +
+                                    '<li> Category: ' + category + '</li>' +
+                                    '<li> Ingredient1: ' + ingredient1 + '</li>' +
+                                    '<li> Measurement1: ' + measurement1 + '</li>' +
+                                    '<li> Ingredient2: ' + ingredient2 + '</li>' +
+                                    '<li> Measurement2: ' + measurement2 + '</li>' +
+                                    '<li> Ingredient3: ' + ingredient3 + '</li>' +
+                                    '<li> Measurement3: ' + measurement3 + '</li>' +
+                                    '<li> Favorite: ' + favorite + '</li>' +
+                                    '<li> Prep: ' + prep + '</li>' +
+                                    '<li> Date: ' + date + '</li>' +
+                                    '<li> Instructions: ' + instructions + '</li>' +
+                                    '</ul>' +
+                            '</div>'
+                        ).appendTo("#dataXML");
+                    }
+                )}
+    });
+
+$('#dataJSON').on('pageinit', function(){
+    var jsonData = function () {
+        $.ajax({
+            type: "GET",
+            url: "xhr/data.json",
+            dataType: "json",
+            success: function (result) {
+
+                for ( var i = 0, len = result.recipes.length; i < len; i++ ) {
+                    var item = result.recipes[i];
+
+                    $( ' ' +
+                        '<div class="contentJSON">' +
+                        '<ul>' +
+                        '<li>' + item.name[0]  + " " + item.name[1] + '</li>' +
+                        '<li>' + item.category[0] + " " + item.category[1] + '</li>' +
+                        '<li>' + item.ingredient1[0] + " " + item.ingredient1[1] + '</li>' +
+                        '<li>' + item.measurement1[0] + " " + item.measurement1[1] + '</li>' +
+                        '<li>' + item.ingredient[0] + " " + item.ingredient2[1] + '</li>' +
+                        '<li>' + item.measurement2[0] + " " + item.measurement2[1] + '</li>' +
+                        '<li>' + item.ingredient3[0] +  " " + item.ingredient3[1] + '</li>' +
+                        '<li>' + item.measurement3[0] + " " + item.measurement3[1] + '</li>' +
+                        '<li>' + item.favorite[0] + " " + item.favorite[1] + '</li>' +
+                        '<li>' + item.prep[0] + " " + item.prep[1] + '</li>' +
+                        '<li>' + item.date[0] + " " + item.date[1] + '</li>' +
+                        '<li>' + item.instructions[0] + " " + item.instructions[1] + '</li>' +
+                        '</ul>' +
+                        '<hr />' +
+                        '</div>'
+                    ).appendTo("#dataJSON");
+                }
+            }
+
+        });
+    }
+
+$('#dataCSV').on('pageinit', function(){
+    var csvData = function () {
+        $.ajax({
+            type: "GET",
+            url: "xhr/data.csv",
+            dataType: "text",
+            success: function (result) {
+                var lines = result.split("\n");
+                var rowCSV = line[0];
+                var colCSV = rowCSV.split(",");
+                for(var lineNum = 1; lineNum < lines.length; lineNum++){
+                    var row = lines[lineNum];
+                    var columns = row.split(",");
+                    $(' '+
+                        '<div class="contentCSV">' +
+                        '<ul>' +
+                        '<li>' + colCSV[0] + " " + columns[0] + '</li>' +
+                        '<li>' + colCSV[1] + " " + columns[1] + '</li>' +
+                        '<li>' + colCSV[2] + " " + columns[2] + '</li>' +
+                        '<li>' + colCSV[3] + " " + columns[3] + '</li>' +
+                        '<li>' + colCSV[4] + " " + columns[4] + '</li>' +
+                        '<li>' + colCSV[5] + " " + columns[5] + '</li>' +
+                        '<li>' + colCSV[6] + " " + columns[6] + '</li>' +
+                        '</ul>' +
+                        '</div>'
+                    ).appendTo("#dataCSV");
+
+                }
+
+            }
+
+        });
+    }
+
+});
 		
 $('#addItem').on('pageinit', function(){
 
