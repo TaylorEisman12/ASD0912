97057908c69a39d0f8e0d44ac81961291796bbf9
Taylor Eisman <TaylorEisman@fullsail.edu>
2012-09-13 19:32:22 -0500

    Setting up main.js and index.html

---
+++ asdcouch/asdcouch/app/_attachments/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$.ajax({
+		"url": "_view/recipes",
+		"dataType": "json",
+		"success": function(data) {
+			$.each(data.rows, function(index, recipe){
+				var recipeName = recipe.value.recipeName;
+				var category = recipe.value.category;
+				var ingredient1 = recipe.value.ingredient1;
+				var measurement1 = recipe.value.measurement1;
+				var ingredient2 = recipe.value.ingredient2;
+				var measurement2 = recipe.value.measurement2;
+				var addToFav = recipe.value.addToFav;
+				var prepTime = recipe.value.prepTime;
+				var dateAdded = recipe.value.dateAdded;
+				var instructions = recipe.value.instructions;
+				$('#recipeList').append(
+					$('<li>').text(category)
+				);
+			});
+		}
+	});
+});
---
+++ asdcouch/asdcouch/app/views/recipes/map.js
@@ -0,0 +1,15 @@
+function (doc){
+	if (doc._id.substring(0 ,7) != "design/"){
+	}emit(doc._id, {
+	"recipeName": doc.title,
+	"category": doc.category,
+	"ingredient1": doc.ingredient1,
+	"measurement1": doc.measurement1,
+	"ingredient2": doc.ingredient2,
+	"measurement2": doc.measurement2,
+	"addToFav": doc.addToFav,
+	"prepTime": doc.prepTime,
+	"dateAdded": doc.dateAdded,
+	"instructions": doc.instructions
+	});
+};
